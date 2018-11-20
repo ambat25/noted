00c26306e0e1c8d415e09959ad3e1ef9b19da881
ambat25 <25ambat25@gmail.com>
2018-11-20 10:54:05 +0100

    added note datails

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import '@fortawesome/fontawesome-free/css/all.css';
 import Newnote from './components/Newnote';
 import Navbar from './components/Navbar';
 import Notes from './components/Notes';
+import NoteDetails from './components/NoteDetails';
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
 				<Navbar />
 				<Newnote />
+				<NoteDetails />
 				<Notes />
 			</div>
 		);
