f9bcb404409cedeb96cc7e0bbddbd739da570df6
ambat25 <25ambat25@gmail.com>
2018-11-20 15:06:23 +0100

    Updates

---
+++ precache-manifest.afa6a59a8cf98e31119d5cfc7e814f4d.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf6008d396082c09c3dd4907de9f3941",
+    "url": "/noted/static/media/fa-regular-400.cf6008d3.woff2"
+  },
+  {
+    "revision": "4e42d6ffecdc293f2bfd",
+    "url": "/noted/static/css/main.6539987f.chunk.css"
+  },
+  {
+    "revision": "1d220cf9da36861171fa90d3c164f4d3",
+    "url": "/noted/static/media/fa-solid-900.1d220cf9.svg"
+  },
+  {
+    "revision": "0ffe58b9c3e5f5f18814",
+    "url": "/noted/static/js/1.0ffe58b9.chunk.js"
+  },
+  {
+    "revision": "31e9406ff309d1742a15",
+    "url": "/noted/static/js/runtime~main.31e9406f.js"
+  },
+  {
+    "revision": "1d709bdd283d0097b7fdb22a75e1f04a",
+    "url": "/noted/static/media/check.1d709bdd.svg"
+  },
+  {
+    "revision": "f2594ef62455697f61dc99862c19afba",
+    "url": "/noted/static/media/fa-brands-400.f2594ef6.eot"
+  },
+  {
+    "revision": "ab673bac3a9e36cb1d4e54777c413fe3",
+    "url": "/noted/static/media/fa-brands-400.ab673bac.ttf"
+  },
+  {
+    "revision": "9d56249d09070f656a1c52e566448f0d",
+    "url": "/noted/static/media/fa-brands-400.9d56249d.woff"
+  },
+  {
+    "revision": "c862e94cbef741d18838774587e3c49d",
+    "url": "/noted/static/media/fa-brands-400.c862e94c.svg"
+  },
+  {
+    "revision": "491a96d8188670aff6f5e8ae4e29ed6a",
+    "url": "/noted/static/media/fa-regular-400.491a96d8.eot"
+  },
+  {
+    "revision": "4e42d6ffecdc293f2bfd",
+    "url": "/noted/static/js/main.4e42d6ff.chunk.js"
+  },
+  {
+    "revision": "b5a61b229c9c92a6ac21f5b0e3c6e9f1",
+    "url": "/noted/static/media/fa-regular-400.b5a61b22.svg"
+  },
+  {
+    "revision": "20b351a6af2d523589fd193785e7d7f0",
+    "url": "/noted/static/media/fa-solid-900.20b351a6.eot"
+  },
+  {
+    "revision": "e6ff1c5d13b7786272782029310c3615",
+    "url": "/noted/static/media/fa-regular-400.e6ff1c5d.ttf"
+  },
+  {
+    "revision": "921f1150167369cf4c400135a4905728",
+    "url": "/noted/static/media/fa-brands-400.921f1150.woff2"
+  },
+  {
+    "revision": "4f8bb28722068f7b666582a39c7035b8",
+    "url": "/noted/static/media/fa-regular-400.4f8bb287.woff"
+  },
+  {
+    "revision": "1dc5b6dd4bf409a6f919be38603f76a0",
+    "url": "/noted/static/media/fa-solid-900.1dc5b6dd.woff2"
+  },
+  {
+    "revision": "997f6b1819184c54248de33c81b7bb0c",
+    "url": "/noted/static/media/fa-solid-900.997f6b18.woff"
+  },
+  {
+    "revision": "63726a69fa60cb67459140ccaf679f96",
+    "url": "/noted/static/media/fa-solid-900.63726a69.ttf"
+  },
+  {
+    "revision": "0ffe58b9c3e5f5f18814",
+    "url": "/noted/static/css/1.3f168a7f.chunk.css"
+  },
+  {
+    "revision": "3e745a798c41290523238cc1d4177b23",
+    "url": "/noted/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+
+importScripts(
+  "/noted/precache-manifest.afa6a59a8cf98e31119d5cfc7e814f4d.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/noted/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
